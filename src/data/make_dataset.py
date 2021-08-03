5893bf79977df295aac30fae3b17a1038be8d2a4
arcweld <smweld.af@gmail.com>
2021-08-03 07:05:36 -0400

    ditto

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -9,7 +9,7 @@ DATA_PATH = os.path.join('data','raw')
 datafile = DATA_PATH + '/X_train_sat6.csv'
 
 X_tr = pd.DataFrame([])
-for df in pd.read_csv(datafile, iterator=True, chunksize=500):
+for df in pd.read_csv(datafile, iterator=True, chunksize=5000):
     X_tr = X_tr.append(df)
     print('*', end=' ')
 
