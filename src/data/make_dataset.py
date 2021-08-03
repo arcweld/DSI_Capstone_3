1a94ee8cfd8f9aee923f5863259268869f67a067
arcweld <smweld.af@gmail.com>
2021-08-03 07:21:26 -0400

    added logging to make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,14 +5,18 @@ import os
 import sys
 import logging
 
+
 DATA_PATH = os.path.join('data','raw')
 datafile = DATA_PATH + '/X_train_sat6.csv'
 
+iter = 0
 X_tr = pd.DataFrame([])
 for df in pd.read_csv(datafile, iterator=True, chunksize=5000):
     X_tr = X_tr.append(df)
-    print('*', end=' ')
+    logging.debug(f'appended, iteration {iter} = {5000*(iter+1)} rows')
+    iter += 1
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
     print(df.head())
