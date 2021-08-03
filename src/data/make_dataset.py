930f1fc56c108c968c4f105ba0c66cc09ece48be
Steven Weld <smweld.af@gmail.com>
2021-08-03 12:32:56 +0000

    misc updates

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -11,12 +11,13 @@ datafile = DATA_PATH + '/X_train_sat6.csv'
 
 iter = 0
 X_tr = pd.DataFrame([])
-for df in pd.read_csv(datafile, iterator=True, chunksize=5000):
+for df in pd.read_csv(datafile, iterator=True, chunksize=200, low_memory=False):
     X_tr = X_tr.append(df)
-    logging.debug(f'appended, iteration {iter} = {5000*(iter+1)} rows')
+    print(f'iter {iter} / 1620')
+    logging.debug(f'appended, iteration {iter} = {200*(iter+1)} rows')
     iter += 1
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+    logging.basicConfig(filename='./app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
     print(df.head())
