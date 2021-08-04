c1e8d5b7eb0905fa6fd5fbb9a908d86d9a9afd4b
arcweld <smweld.af@gmail.com>
2021-08-04 11:00:16 -0400

    reshaped X data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -16,14 +16,16 @@ DATA_PATH = os.path.join('data','raw')
 
 def fetch_training_data(data_path=DATA_PATH, nrows=None, chunksize=2000):
     X_tr_datafile = data_path + '/X_train_sat6.csv'
-    X_tr = mem_managed_df_read(X_tr_datafile, nrows, chunksize)
+    X = mem_managed_df_read(X_tr_datafile, nrows, chunksize)
+    X_tr = X.values.reshape(-1,28,28,4)
     y_tr_datafile = data_path + '/y_train_sat6.csv'
     y_tr = pd.read_csv(y_tr_datafile, header=None, nrows=nrows)
     return X_tr, y_tr
 
 def fetch_testing_data(data_path=DATA_PATH, nrows=None, chunksize=2000):
     X_test_datafile = data_path + '/X_test_sat6.csv'
-    X_test = mem_managed_df_read(X_test_datafile, nrows, chunksize)
+    X = mem_managed_df_read(X_test_datafile, nrows, chunksize)
+    X_test = X.values.reshape(-1,28,28,4)
     y_test_datafile = data_path + '/y_test_sat6.csv'
     y_test = pd.read_csv(y_test_datafile, header=None, nrows=nrows)
     return X_test, y_test
